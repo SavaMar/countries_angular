230d76460f465e0c22a4d5a77db1f5abddec6763
SavaMar <isterikamaus3@gmail.com>
2017-04-06 12:40:22 +0200

    Add correct work to checkboxes

--- script.js
+++ script.js
@@ -1,175 +1,57 @@
 angular.module('myApp', ['ui.bootstrap'])
 	.controller("countriesCtrl", function($scope, $http){
-		var data_f;
 		$http.get('/node_modules/countries-list/countries.json').success(function(data) {
 		    $scope.continents = data.continents;
 		    $scope.countries = data.countries;
 		  });
 
+		  // $scope.q = []
 
-		// $scope.cbChecked = function(continent){
-		//     $scope.allSelected = true;
-		//     angular.forEach($scope.countries, function(v, k) {
-		//       if(!v.checked){
-		//         // $scope.allSelected = false;
-		//         $scope.toggleAll(continent)
-		//       }
-		//     });
-		//   }
+	$scope.selectCountries = function(country, continent, continent_name){
+		angular.forEach($scope.continents, function(val,key){
 
-		// $scope.toggleAll = function(continent) {
-			
-		//     var bool = true;
-		//     if ($scope.allSelected) {
-		//       bool = false;
-		//     }
-		//     angular.forEach($scope.countries, function(v, k) {
-		// 	    if(v.continent == continent) {
-		// 	    	// console.log( continent);
-		// 	    	// console.log( k);
-		// 		    v.checked = bool;
-		// 		    $scope.allSelected = bool;
-		// 	    }
-		//     });
-		//   }
-				//======================================
-
-		  // $scope.$watch('countries', function(countries, continents){
-		  //   var total_selected = 0;
-
-		  //   angular.forEach(continents, function(val,key){
-		    
-		  //     continent.countries_selected = 0;
-		    
-		  //     angular.forEach(continent.countries, function(country){
-		      
-		  //       total_selected += country.selected ? 1 : 0
-		        
-		  //       continent.countries_selected += country.selected ? 1 : 0
-		        
-		  //       if (continent.countries_selected == continent.countries.length) {
-		  //         continent.selected = true;
-		  //       } else {
-		  //         continent.selected = false;
-		  //       }
-		        
-		  //     });
-		      
-		  //   });
-		    
-		  //   $scope.select_all = function(continent){
-		  //     continent.selected = true;
-		  //   }
-		    
-		  //   $scope.total_selected = total_selected;
-		    
-		  // }, true);
-
-		  //========================================
-
-		//   //========================================
-
-// 		$scope.$watch('countries', function(countries, continents){
-// 			console.log(continents)
-// 			var total_selected = 0;
-// 			var total_countries;
-// 			var c = 0;
-				
-// 			angular.forEach(continents, function(val,key){
-				
-// 				var continent_countries_selected = 0;
-// 				total_countries = 0;
-// 				c+=1;
-// 				angular.forEach(countries, function(country){
-					
-
-// 					if(country.continent == key){
-// 						total_countries += 1;
-// 						total_selected += country.selected ? 1 : 0
-// 						// total_selected += country.selected ? 1 : 0
-// 						continent_countries_selected += country.selected ? 1 : 0
-
-// 						// console.log(total_countries)
-// 						// console.log(continent_countries_selected)
-// 						// if (continent_countries_selected == total_countries) {
-// 				  //         console.log(total_countries)
-// 				  //       } else {
-// 				  //         val.selected = false;
-// 				  //       }
-// 					}
-// 			// 		continent_countries_selected = 0;
-
-
-					
-// 			// 		angular.forEach(continents, function(val,key){
-// 			// 			if(country.continent == key && countrie.select){
-// 			// // 			// console.log(lo) 
-
-// 			// // 			if(country.continent == key && countrie.select){
-// 			// // 				total_selected += country.selected ? 1 : 0
-// 			// // 				continent_countries_selected += country.selected ? 1 : 0
-
-// 			// // 				// console.log(continent_countries_selected)
-// 			// // 				if (continent_countries_selected == 5) {
-// 			// // 		          $scope.continents[key] = true;
-// 			// // 		        } else {
-// 			// // 		          val.selected = false;
-// 			// // 		        }
-// 			// // 			}
-// 			// // // 			total_selected += country.selected ? 1 : 0
-// 			// // // 			continent.countries_selected += country.selected ? 1 : 0
-
-// 			// // 			// if (continent_countries_selected == continent.countries.length) {
-// 			// // 	  //         continent.selected = true;
-// 			// // 	  //       } else {
-// 			// // 	  //         continent.selected = false;
-// 			// // 	  //       }
-// 				    });
-// 				// console.log(c)
-// 				});
-
-// console.log(c)
-	    
-// 		// // //     $scope.select_all = function(continent){
-// 		// // //       continent.selected = true;
-// 		// // //     }
-		    
-// 		// // //     $scope.total_selected = total_selected;
-// 		}, true);
-
-		$scope.c = function(country, continent, continent_name){
-			// console.log($scope.continent_name = true);
-			// var bool = continent_name;
-
-			angular.forEach($scope.continents, function(val,key){
-				// console.log(val);
-
-				var total_selected = 0;
-				var total_countries = 0;
-				angular.forEach($scope.countries, function(country){
-					if(country.continent == key){
-						total_countries += 1;
-						total_selected += country.selected ? 1 : 0
-					}
-				});
+			var total_selected = 0;
+			var total_countries = 0;
+			angular.forEach($scope.countries, function(country){
 				if(country.continent == key){
-					if(total_selected == total_countries) {
-								console.log(true)
-								country.selected = true;
-								// $scope.continent_name = true;
-					}
+					total_countries += 1;
+					total_selected += country.selected ? 1 : 0
 				}
-				// console.log(total_selected)
 			});
-		}
+			if(country.continent == key){
+				if(total_selected == total_countries) {
+					$('#' + continent_name).prop('checked', true);
+					$scope.val = true;
+				} else {
+					$('#' + continent_name).prop('checked', false);
+				}
+			}
+		});
+	}
 
-		$scope.parentChange = function(continent, continent_name) {
-			var bool = continent_name;
-			angular.forEach($scope.countries, function(v, k) {
-			    if(v.continent == continent) {
-				    v.selected = bool;
-				    // $scope.continent_name = bool;
-			    }
-		    });   
-		}
-	});
+	$scope.checkedAll = function(continent, continent_name) {
+		var bool = continent_name;
+		var i = 0;
+		var j = 0;
+		angular.forEach($scope.countries, function(v, k) {
+		    if(v.continent == continent) {
+		    	i+=1;
+		    	if(v.selected) {
+		    		j+=1;
+		    	}
+		    }
+	    }); 
+	    if(i != j) {
+    		bool = true
+    	}else {
+    		bool = false
+    	}
+
+	    angular.forEach($scope.countries, function(v, k) {
+	    	if(v.continent == continent) {
+			    v.selected = bool;
+		    }
+	    });
+	}
+});
+
